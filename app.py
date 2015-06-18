73f17b7f7f55c96f93fda26ee2eb5668c0c1f57b
DennisNenov <nycdennen@gmail.com>
2015-06-18 18:50:34 -0400

    Functional auth

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, session, flash, redirect, render_template, request, url_for
+import database
 app = Flask(__name__)
 app.secret_key = 'lmao' 
 
@@ -13,15 +14,33 @@ def login(username):
 def home():
     return render_template('home.html') 
 
-@app.route('/login',methods=['POST','GET'])
+@app.route('/login', methods=["GET","POST"])
 def login():
     error = None
-    if request.method == 'POST':
-        if validLogin(request.form['username'],
-                      request.form['password']):
-            return login(request.form['username'])
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if(database.validateUser(username,password) == False):
+            error = 'Unregistered username or incorrect password'
+            return redirect(url_for('home'))
+        flash("You've logged in successfully")
+        session['username'] = request.form['username']
+        return redirect(url_for('home'))
+    return render_template("login.html")
 
-    return render_template('login.html', error=error)
+@app.route('/signup', methods=["GET","POST"])
+def signup():
+    error = None
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if not database.addUser(username,password):
+            flash("Registered username, too short username, or too short password.")
+            return redirect(url_for('signup'))
+        flash("Great! You've registered! Now you can log in.")
+        return redirect(url_for('login'))
+    return render_template("signup.html")
+
 
 @app.route('/studyguides',methods=['GET','POST'])
 def guides():
