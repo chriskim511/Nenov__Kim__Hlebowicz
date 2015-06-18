b5d0a1a772f708695df144e91fc16f163091bed5
thlebowicz <thomashlebowicz1@gmail.com>
2015-06-18 14:41:03 -0400

    Added login page and mongodb

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask,render_template, session, redirect, request, url_for, g, flash
 from functools import wraps
+from pymongo import MongoClient
 
 app = Flask(_name_)
 app.secret_key = "key"
+
+client = MongoClient()
+db = client['studyData']
+accountDB = db['accounts']
+guideDB = db['guides']
 
 
 @app.route("/")
@@ -16,6 +22,10 @@ def guides():
 @app.route("/stats")
 def stats():
     return render_template("stats.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 
         
